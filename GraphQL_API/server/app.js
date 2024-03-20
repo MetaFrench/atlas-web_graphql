446b15c826052caf3f7b5f38deeece228246b67d
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-03-20 13:46:57 -0500

    app

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,1 +1,26 @@
-dd
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors()); // allow cross-origin requests
+
+// connect MongoDB Atlas database
+const mongoDBAtlasUri = 'mongodb+srv://hayes28:Password@atlascluster.tqd66ql.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(mongoDBAtlasUri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for request on port 4000');
+});
